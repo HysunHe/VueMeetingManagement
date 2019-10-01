096c3009b398cb09a92cac0e80152a99806dc4df
HysunHe <hysunhe@foxmail.com>
2019-10-01 18:08:27 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,212 @@
+<template>
+  <div id="app">
+    <!-- header -->
+    <div>
+      <el-header height="85px" style="text-align:center;">
+        <span style="font-size:36px;">首页</span>
+        <div style="float: left;height: 83px;color:#ffffff; font-size:45px; background-color: #205796; width:182px; margin-left:-20px; text-align: center; align:center;border-bottom: 2px solid #091c30;">
+           <img  src="../assets/logo.png">
+          </div>
+        <div style="float: right;color:#fff; font-size: 12px;margin-right:50px;">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px;color:#fff;"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>
+                <div @click="jumpTo()">
+                  <span style="color: #555;font-size: 14px;">个人信息</span>
+                </div>
+              </el-dropdown-item>
+              <el-dropdown-item>
+                <div @click="jumpTo('/user/changepwd')">
+                  <span style="color: #555;font-size: 14px;">修改密码</span>
+                </div>
+              </el-dropdown-item>
+              <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span @click="aaa()">{{userName}}</span>
+        </div>
+        <div class="clear"></div>
+      </el-header>
+    </div>
+    <!-- body -->
+    <el-container  class="bg_home">
+      <el-aside width="182px" style="background-color: #205796;">
+          <div class="menu-button">
+            <span style="cursor:pointer;">
+              <img  style="padding-top:25px;" src="../assets/tasks.png">
+              <span style="display:block; font-size:18px; font-weight:bold;">我的任务</span>
+            </span>
+          </div>
+          <div class="menu-button">
+            <span style="cursor:pointer;">
+              <img  style="padding-top:25px;" src="../assets/sm.png">
+              <span style="display:block; font-size:18px; font-weight:bold;">供应商管理</span>
+            </span>
+          </div>        
+        <el-menu style="background-color: #16181D;" class="el-menu-vertical-demo" :default-active="$route.path" router :unique-opened='true'
+          @select="handleSelect" :collapse='iscloseNav'>
+          <template v-for="(item,index) in menuList">
+            <el-submenu :index="index+''" :key="index">
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span slot="title" style="color:#b3c0d1">{{item.name}}</span>
+              </template>
+              <el-menu-item v-for="(ll,ii) in item.snippet" :key="ii" :index="'/'+ll.urlf.split('.')[0]">{{ll.title}}</el-menu-item>
+            </el-submenu>
+          </template>
+        </el-menu>
+      </el-aside>
+      
+      <!-- 右侧路由信息 -->
+      <el-container style="min-width:1605px;">
+        <el-main style="padding:0;">
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      defaultActiveIndex: ["0"],
+      menuList: [],
+      userName: "",
+      collapsed: false,
+      iscloseNav: false,
+      welcome: true
+    };
+  },
+  mounted2() {
+    var w = window.innerWidth;
+    if (w < 500) {
+      this.welcome = false;
+    }
+    this.userName = getCookie("username");
+    var url = window.location.href;
+    if (url.split("#")[1] == "/") {
+    } else {
+      this.collapsed = true;
+    }
+    var tt = this;
+    // if (getCookie("token")) {
+      this.$http
+        .get("../../static/JSON/GetMenus.json"
+        // , {
+        //   params: {
+        //     Token: getCookie("token")
+        //   }
+        // }
+        )
+        .then(
+          function(response) {
+            var status = response.data.Status;
+            if (status === 1) {
+              this.menuList = response.data.Result;
+              localStorage.setItem(
+                "menulist",
+                JSON.stringify(response.data.Result)
+              );
+            } else if (status === 40001) {
+              this.$message({
+                showClose: true,
+                type: "warning",
+                message: response.data.Result
+              });
+              setTimeout(() => {
+                tt.$router.push({
+                  path: "/login"
+                });
+              }, 1500);
+            }
+          }.bind(this)
+        )
+        .catch(
+          function(error) {
+            this.$notify.error({
+              title: "错误",
+              message: "错误：请检查网络"
+            });
+          }.bind(this)
+        );
+    // } else {
+    //   this.$message({
+    //     showClose: true,
+    //     type: "warning",
+    //     message: "请先登陆"
+    //   });
+    //   setTimeout(() => {
+    //     tt.$router.push({
+    //       path: "/login"
+    //     });
+    //   }, 1500);
+    // }
+  },
+  methods: {
+    // 	index: 选中菜单项的 index, indexPath: 选中菜单项的 index path
+    handleSelect(index) {
+      this.collapsed = true;
+      // this.defaultActiveIndex = [index];
+      // console.log(this.$route.path);
+    },
+    // 个人中心  修改密码
+    jumpTo(url) {
+      this.defaultActiveIndex = url;
+      this.$router.push(url);
+    },
+    // 退出
+    logout() {
+      let that = this;
+      this.$confirm("确认退出吗?", "提示", {
+        confirmButtonClass: "el-button--warning"
+      })
+        .then(() => {
+          //确认
+          that.loading = true;
+          delCookie("token");
+          this.$router.push("/login");
+        })
+        .catch(() => {});
+    },
+    closeNav() {
+      this.iscloseNav = !this.iscloseNav;
+      if (this.iscloseNav) {
+        $(".el-aside").css({
+          width: "60px"
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* @import "../../static/css/index.css"; */
+
+.bg_home {
+  background: url("../assets/bg_home.png") no-repeat;  
+  background-size: 100% 100%;
+}
+
+.el-header {
+  background-color: #587a97;
+  color: #ffffff;
+  line-height: 85px;
+}
+
+.el-menu--popup {
+  background-color: #16181d;
+}
+
+.menu-button {
+  color:white;
+  text-align: center;
+  margin:0;
+  border-bottom: 1px solid #091c30;
+  width:100%;
+  height:120px;
+}
+</style>
---
+++ src/components/MyTask.vue
@@ -0,0 +1,205 @@
+<template>
+	<div> 
+		<div class="top-msg-region">
+			<el-row  style="font-size:20px;">
+				<el-col :span="12" style="margin-top: 45px;">
+					<span style="padding-left:52px;" >最近会议时间:  </span><span>2019/09/24 16:08:23 </span>
+				</el-col>
+				<el-col :span="12" style="margin-top: 45px;">
+					<span>未加入会议议题: </span><span style="font-size:20px; color:#ffed27;">年末销售提升方案</span>
+				</el-col>				
+			</el-row>
+		</div>
+
+        <div style="margin-left:52px;">
+            <el-row type="flex" :gutter="50">
+                <el-col :span="24">
+                    <el-card class="box-card">
+                        <div class="menu-button">
+                            <span style="cursor:pointer;">
+                            <img  style="padding-top:25px;" src="../assets/mc.png">
+                            <span style="display:block; ">会议创建</span>
+                            </span>
+                        </div>
+                    </el-card>
+                    <el-card class="box-card" style="margin-left:86px;">
+                        <div class="menu-button">
+                            <span style="cursor:pointer;">
+                            <img  style="padding-top:25px;" src="../assets/mo.png">
+                            <span style="display:block; ">会议组织</span>
+                            </span>
+                        </div>
+                    </el-card>
+                    <el-card class="box-card" style="margin-left:86px;">
+                        <div class="menu-button">
+                            <span style="cursor:pointer;">
+                            <img  style="padding-top:25px;" src="../assets/mq.png">
+                            <span style="display:block; ">会议查询</span>
+                            </span>
+                        </div>
+                    </el-card>
+                </el-col>					
+            </el-row>
+
+            <el-row type="flex" style="margin-top:60px;">
+                <el-col :span="24">
+                    <el-card class="table-card" style="margin-right:22px;">
+                        <div slot="header" class="clearfix">
+                            <span style="font-size:24px; color:#333333; font-weight:600;">上会申请（当前有<span style="color:#ff200f;">30</span>会议申请等待安排）</span>
+                        </div>
+                        <div>
+                            <el-table
+                                :data="tableData"
+                                stripe
+                                header-cell-style="font-weight:bold; color:#333333;"
+                                style="width: 100%; font-size:18px; color:#333333;">
+                                <el-table-column
+                                prop="date"
+                                label="日期"
+                                width="180">
+                                </el-table-column>
+                                <el-table-column
+                                prop="name"
+                                label="姓名"
+                                width="180">
+                                </el-table-column>
+                                <el-table-column
+                                prop="address"
+                                label="地址">
+                                </el-table-column>
+                            </el-table>
+                        </div>
+                        <div class="bottom clearfix">
+                            <time class="time">{{ currentDate }}</time>
+                            <el-button type="text" class="button">操作按钮</el-button>
+                        </div>
+                    </el-card>
+                   <el-card class="table-card" style="margin-left:22px;">
+                        <div slot="header" class="clearfix">
+                            <span style="font-size:24px; color:#333333; font-weight:600;">上会申请（当前有<span style="color:#ff200f;">30</span>会议申请等待安排）</span>
+                        </div>
+                        <div>
+                            <el-table
+                                :data="tableData"
+                                stripe
+                                header-cell-style="font-weight:bold; color:#333333;"
+                                style="width: 100%; font-size:18px; color:#333333;">
+                                <el-table-column
+                                prop="date"
+                                label="日期"
+                                width="180">
+                                </el-table-column>
+                                <el-table-column
+                                prop="name"
+                                label="姓名"
+                                width="180">
+                                </el-table-column>
+                                <el-table-column
+                                prop="address"
+                                label="地址">
+                                </el-table-column>
+                            </el-table>
+                        </div>
+                        <div class="bottom clearfix">
+                            <time class="time">{{ currentDate }}</time>
+                            <el-button type="text" class="button">操作按钮</el-button>
+                        </div>
+                    </el-card>
+                </el-col>			
+            </el-row>
+        </div>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			 return {
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+		}
+	}
+</script>
+<style type="text/css"> 
+		.text {
+			font-size: 14px;
+		}
+		.item {
+			padding: 18px 0;
+		}
+		.box-card {
+			width:438px;
+			height:314px;
+            display: inline-table;
+		}
+		.table-card {
+			width:723px;
+			max-height:476px;
+            min-height: 400px;
+            display: inline-table;
+            border: 1px solid;
+            border-color: #4f718a;
+		}
+		.el-row {
+			margin-bottom: 30px;
+			&:last-child {
+			margin-bottom: 0;
+			}
+		}
+		.el-col {
+			border-radius: 4px;
+		}
+		.top-msg-region {
+			min-height:111px;
+			background: #505f67;
+			color:white;
+		}
+		.grid-content {
+			border-radius: 4px;
+			min-height: 36px;
+		}
+        .menu-button {
+            color:#333333;
+            font-size:34px;
+            text-align: center;
+            margin:0;
+            width:100%;
+            height:120px;
+        }
+        .bottom {
+            margin-top: 20px;
+        }
+
+        .button {
+            padding: 0;
+            float: right;
+            background: #2595cf;
+            width:144px;
+            height:52px;
+            font-size:18px;
+            color: #ffffff;
+        }
+        .clearfix:before,
+        .clearfix:after {
+            display: table;
+            content: "";
+        }
+        .clearfix:after {
+            clear: both
+        }
+</style>
