ca6e2cf11fb13bd7fdc655caf44e69839775b326
HysunHe <hysunhe@foxmail.com>
2019-10-10 12:45:42 +0800

    impl

--- src/components/MeetingOrganize.vue
+++ src/components/MeetingOrganize.vue
@@ -27,6 +27,24 @@
             </el-select>
         </div>
 
+       <el-dialog title="添加主题" :visible.sync="dialogVisible">
+            <el-form :model="form">
+                <el-form-item label="活动名称" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域" :label-width="formLabelWidth">
+                <el-select v-model="form.region" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogFormVisible = false">取 消</el-button>
+                <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+        </el-dialog>
+
         <div id="topicsTable" style="margin-top:14px;">
             <el-table
                 :data="meetingTopics"
@@ -87,7 +105,19 @@
                 selectedMeeting: {},
                 meetingTopics: [],
                 totalTopics: 0,
-                showMeetingSelBox: false
+                showMeetingSelBox: false,
+                dialogVisible: false,
+                form: {
+                    name: '',
+                    region: '',
+                    date1: '',
+                    date2: '',
+                    delivery: false,
+                    type: [],
+                    resource: '',
+                    desc: ''
+                },
+                formLabelWidth: '120px'
             }
         },
         created() {
